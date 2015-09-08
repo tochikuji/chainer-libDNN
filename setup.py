f95020d044a01ee5198cfe7b95cbe61076878679
Aiga SUZUKI <ai-suzuki@aist.go.jp>
2015-09-08 15:34:00 +0900

    setup.py added, to became a package
    directory structure was changed.
    copy libcnn/*.py on your current when using previously

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+
+setup(name='Chainer-libCNN',
+      version='2.0.0',
+      description='General utilities for neural networs with chainer',
+      author='Aiga SUZUKI',
+      author_email='ai-suzuki@aist.go.jp',
+      url='http://git.tochikuji.com/tochikuji/chainer-libcnn',
+      packages=find_packages()
+      )
